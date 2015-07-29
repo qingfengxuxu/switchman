ece88d78787e688a3906a472b62c8c8168d8fc02
jrubio <jordi.rubio@immobilienscout24.de>
2015-07-29 11:40:13 +0200

    Make trailing slash in urls optional

--- src/main/java/de/is24/common/infrastructure/config/Web.java
+++ src/main/java/de/is24/common/infrastructure/config/Web.java
@@ -1,6 +1,7 @@
 package de.is24.common.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,6 +12,11 @@ public class Web extends WebMvcConfigurerAdapter {
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/login").setViewName("login");
     registry.addViewController("/").setViewName("index");
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.setUseTrailingSlashMatch(true);
   }
 
 }
